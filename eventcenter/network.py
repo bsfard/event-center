2daee56af73f3256ad76c546d656485282383ec1
bsfard <38926677+bsfard@users.noreply.github.com>
2022-11-12 00:35:03 -0800

    Adding session to api call to make call using the session instead of a non-session based request call.
    Adding ability to provide headers when making api call (default will use the defined HEADERS constant).
    
    Adding methods to validate successful response (status=200) and error class to raise if it's not successful.
    
    Adding method to make a GET api call.
    
    Adding method to remove unset parameters before making api call.

--- eventcenter/network.py
+++ eventcenter/network.py
@@ -37,13 +37,51 @@ class FlaskAppRunner(threading.Thread):
 
 class APICaller:
     @staticmethod
-    def make_post_api_call(url: str, body: Dict[str, Any], is_suppress_connection_error: bool = False):
+    def make_post_api_call(url: str, body: Dict[str, Any], headers: Dict[str, Any] = None,
+                           session: requests.Session = None,
+                           is_suppress_connection_error: bool = False) -> requests.Response:
+        headers = headers if headers else HEADERS
+
         try:
-            response = requests.post(url, json=body, headers=HEADERS)
-            return response if response.status_code == 200 else {}
+            if session:
+                response = session.post(url, json=body, headers=headers)
+            else:
+                response = requests.post(url, json=body, headers=headers)
+
+            APICaller.__validate_response_ok(url, response)
+            return response
+
         except requests.exceptions.ConnectionError as e:
             if not is_suppress_connection_error:
                 raise ApiConnectionError(url, body, e)
+
+    @staticmethod
+    def make_get_call(url: str, params: Dict[str, Any], headers: Dict[str, Any] = None,
+                      session: requests.Session = None,
+                      is_suppress_connection_error: bool = False) -> requests.Response:
+        headers = headers if headers else HEADERS
+        params = APICaller.__remove_empty_params(params)
+        try:
+            if session:
+                response = session.get(url, params=params, headers=headers)
+            else:
+                response = requests.get(url, params=params, headers=headers)
+
+            APICaller.__validate_response_ok(url, response)
+            return response
+
+        except requests.exceptions.ConnectionError as e:
+            if not is_suppress_connection_error:
+                raise ApiConnectionError(url, {}, e)
+
+    @staticmethod
+    def __remove_empty_params(params):
+        return {key: value for (key, value) in params.items() if value}
+
+    @staticmethod
+    def __validate_response_ok(url: str, response):
+        if response.status_code != 200:
+            raise BadResponseStatusError(url, response)
 
 
 class ApiConnectionError(NotifiableError):
@@ -57,3 +95,18 @@ class ApiConnectionError(NotifiableError):
             'body': body
         }
         super().__init__(message, error, payload, exception)
+
+
+class BadResponseStatusError(NotifiableError):
+    """Raised when response code from an API call is not 200"""
+
+    def __init__(self, url: str, response: requests.Response):
+        message = f'\nURL: {url}'
+        error = 'bad_response_error'
+        payload = {
+            'message': message,
+            'url': url,
+            'status_code': response.status_code,
+            'response': response
+        }
+        super().__init__(message, error, payload)
