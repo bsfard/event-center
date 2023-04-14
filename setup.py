e6b4ef475e22a9807676310ae6d8823eb1fe9a7d
bsfard <38926677+bsfard@users.noreply.github.com>
2023-04-13 18:51:16 -0700

    Updating event-dispatch component usage to v0.0.5.
    Bumping this component's version to 0.0.5.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='eventcenter',
-    version='0.0.3',
+    version='0.0.5',
     author='Ben Sfard',
     author_email='bsfard@gmail.com',
     url='https://github.com/bsfard/event-center',
@@ -11,6 +11,6 @@ setup(
     install_requires=[
         'requests==2.28.1',
         'Flask==2.2.2',
-        'eventdispatch @ git+https://github.com/bsfard/event-dispatch.git@v0.0.4'
+        'eventdispatch @ git+https://github.com/bsfard/event-dispatch.git@v0.0.5'
     ]
 )
